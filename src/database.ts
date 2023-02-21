f8c6ea93cd5b5433f9f1c3964c0087936e3984f3
vitornicchio <vitor@imaginationmedia.com>
2023-02-21 08:25:48 -0300

    Create transactions table

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,15 @@
-import { knex as setupKnex } from 'knex'
+import { Knex, knex as setupKnex } from 'knex'
 
-export const knex = setupKnex({
+export const knexConfig: Knex.Config = {
   client: 'sqlite',
   connection: {
     filename: './db/app.db',
   },
-})
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
+
+export const knex = setupKnex(knexConfig)
