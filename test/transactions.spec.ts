ca853ba217b65f520f9da616d4c27c43f477189b
vitornicchio <vitor@imaginationmedia.com>
2023-03-06 21:05:23 -0300

    Test list of transactions

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,5 +1,5 @@
 import request from 'supertest'
-import { afterAll, beforeAll, describe, it } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import { app } from '../src/app'
 
 describe('Transactions Routes', () => {
@@ -11,7 +11,7 @@ describe('Transactions Routes', () => {
     await app.close()
   })
 
-  it('User should be able to create a transaction', async () => {
+  it('should be able to create a transaction', async () => {
     await request(app.server)
       .post('/transactions')
       .send({
@@ -20,5 +20,30 @@ describe('Transactions Routes', () => {
         type: 'credit',
       })
       .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Transaction 1',
+        amount: 100,
+        type: 'credit',
+      })
+      .expect(201)
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'Transaction 1',
+        amount: 100,
+      }),
+    ])
   })
 })
